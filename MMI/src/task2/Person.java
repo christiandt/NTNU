6a68e1ec1a92360755a7fa2d886e4948bda838db
Christian DT <christian@dhcp-029103.wlan.ntnu.no>
2013-01-25 09:58:11 +0100

    Task 2 added

---
+++ MMI/src/task2/Person.java
@@ -0,0 +1,58 @@
+package task2;
+
+public class Person {
+	private String name;
+	private Gender gender;
+	private String dateOfBirth;
+	private String email;
+	private int height;
+	
+	
+	public Person(String name){
+		setName(name);
+	}
+
+	
+
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Gender getGender() {
+		return gender;
+	}
+	public void setGender(Gender gender) {
+		this.gender = gender;
+	}
+	public String getDateOfBirth() {
+		return dateOfBirth;
+	}
+	public void setDateOfBirth(String dateOfBirth) {
+		this.dateOfBirth = dateOfBirth;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public int getHeight() {
+		return height;
+	}
+	public void setHeight(int height) {
+		this.height = height;
+	}
+	
+	public void print(){
+		System.out.println("Name: "+this.name);
+		System.out.println("Email: "+this.email);
+		System.out.println("Gender: "+this.gender.toString());
+		System.out.println("Birth: "+this.dateOfBirth);
+		System.out.println("Height: "+this.height);
+	}
+	
+	
+	
+}
